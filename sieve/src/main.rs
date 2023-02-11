73cc665efd0d1e4d5d22d7409adf7a72f10f4e0c
Burucs Adam <burucs.adam@nexius.hu>
2023-02-11 23:44:06 +0100

    Add WIP Atkin sieve algorithm

--- sieve/src/main.rs
+++ sieve/src/main.rs
@@ -25,30 +25,65 @@ fn sieve_of_eratosthenes(n: u32) -> Vec<u32> {
     primes
 }
 
-// WIP
-// fn sieve_of_atkin(limit: u32) -> Vec<u32> {
-//     let mut sieve = vec![false; limit as usize];
-//     if limit > 2 {
-//         sieve[2] = true;
-//     }
-//     if limit > 3 {
-//         sieve[3] = true;
-//     }
-//     let mut x = 1;
-//     while x * x <= limit {
-//         let mut y = 1;
-//         while y * y <= limit {
-//             let mut n = (4 * x * x) + (y * y);
-//             ... todo
-//             y += 1;
-//         }
-//         x += 1;
-//     }
-// }
+// todo fix panicked error
+fn sieve_of_atkin(limit: u32) -> Vec<u32> {
+    let mut sieve = vec![false; limit as usize];
+    if limit > 2 {
+        sieve[2] = true;
+    }
+    if limit > 3 {
+        sieve[3] = true;
+    }
+    let mut x = 1;
+    while x * x <= limit {
+        let mut y = 1;
+        while y * y <= limit {
+            let mut n = (4 * x * x) + (y * y);
+            if n <= limit && (n % 12 == 1 || n % 12 == 5) {
+                sieve[n as usize] = !sieve[n as usize];
+            }
+            n = (3 * x * x) + (y * y);
+            if n <= limit && (n % 12 == 7) {
+                sieve[n as usize] = !sieve[n as usize];
+            }
+            n = (3 * x * x) - (y * y); // panicked at 'attempt to subtract with overflow'
+            if x > y && n <= limit && (n % 12 == 11) {
+                sieve[n as usize] = !sieve[n as usize];
+            }
+            y += 1;
+        }
+        x += 1;
+    }
+    let mut r = 5;
+    while r * r <= limit {
+        if sieve[r as usize] {
+            let mut i = r * r;
+            while i <= limit {
+                sieve[i as usize] = false;
+                i += r * r;
+            }
+        }
+        r += 1;
+    }
+    let mut primes: Vec<u32> = Vec::new();
+    for prime_index in 0..=limit as usize {
+        if sieve[prime_index] {
+            primes.push(prime_index as u32);
+        }
+    }
+    return primes;
+}
 
 fn main() {
     println!("Prime Sieves");
-    let primes = sieve_of_eratosthenes(100);
-    let p = format!("{:?}", &primes);
-    println!("{}", p);
+    println!("***");
+    println!("Sieve of Eratosthenes");
+    let primes_e = sieve_of_eratosthenes(100);
+    let p_eratos = format!("{:?}", &primes_e);
+    println!("{}", p_eratos);
+    println!("***");
+    println!("Sieve of Atkin"); // todo fix panicked error
+    let primes_a = sieve_of_atkin(100);
+    let p_atkin = format!("{:?}", &primes_a);
+    println!("{}", p_atkin);
 }
