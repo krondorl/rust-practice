b84507c9ec43494b7609e4a5710c0770c7953be8
Adam Burucs <adam.burucs@gmail.com>
2023-12-23 15:25:13 +0100

    refactor qe solver to match solutions

--- src/qe_solver/src/main.rs
+++ src/qe_solver/src/main.rs
@@ -32,26 +32,24 @@ enum EquationRoots {
 }
 
 fn solve_qe(a: f32, b: f32, c: f32) -> EquationRoots {
-    let discriminant: f32 = (b*b)-(4.0*a*c);
+    let discriminant: f32 = (b * b) - (4.0 * a * c);
     if discriminant > 0.0 {
-        let x1: f32 = (-b+(((b*b)-(4.0*a*c)).sqrt())) / (2.0*a);
-        let x2: f32 = (-b-(((b*b)-(4.0*a*c)).sqrt())) / (2.0*a);
+        let x1: f32 = (-b + (((b * b) - (4.0 * a * c)).sqrt())) / (2.0 * a);
+        let x2: f32 = (-b - (((b * b) - (4.0 * a * c)).sqrt())) / (2.0 * a);
         EquationRoots::RealRoots(RealRoots {
             root1: x1,
-            root2: x2
+            root2: x2,
         })
     } else if discriminant == 0.0 {
-        let x1: f32 = -b / (2.0*a);
-        EquationRoots::OnlyRoot(OnlyRoot {
-            root: x1,
-        })
+        let x1: f32 = -b / (2.0 * a);
+        EquationRoots::OnlyRoot(OnlyRoot { root: x1 })
     } else if discriminant < 0.0 {
-        let denominator = 2.0*a;
-        let real_part = -b / (2.0*a);
-        let imaginary_part = format!("i((srqt {})/{})", discriminant, denominator);
+        let denominator = 2.0 * a;
+        let real_part = -b / (2.0 * a);
+        let imaginary_result = discriminant.abs().sqrt() / denominator;
         EquationRoots::ImaginaryRoots(ImaginaryRoots {
-            root1: format!("{}+{}", real_part, imaginary_part),
-            root2: format!("{}-{}", real_part, imaginary_part),
+            root1: format!("{real_part} + {imaginary_result}i"),
+            root2: format!("{real_part} - {imaginary_result}i"),
         })
     } else {
         EquationRoots::NoRoots(NoRoots)
@@ -60,14 +58,34 @@ fn solve_qe(a: f32, b: f32, c: f32) -> EquationRoots {
 
 fn print_solver(a: f32, b: f32, c: f32) {
     let solutions = solve_qe(a, b, c);
-    println!("{}x^2 + {}x + {} = 0", a, b, c);
-    println!("{:?}", solutions);
-    println!("");
+    println!("{a}x^2 + {b}x + {c} = 0");
+
+    match solutions {
+        EquationRoots::RealRoots(real_roots) => {
+            println!("Roots are real and distinct");
+            println!("Root1 = {0}", real_roots.root1);
+            println!("Root2 = {0}", real_roots.root2);
+        }
+        EquationRoots::ImaginaryRoots(img_roots) => {
+            println!("Imaginary Roots");
+            println!("Root1 = {0}", img_roots.root1);
+            println!("Root2 = {0}", img_roots.root2);
+        }
+        EquationRoots::OnlyRoot(only_root) => {
+            println!("There is only one root");
+            println!("Root = {0}", only_root.root);
+        }
+        EquationRoots::NoRoots(_) => println!("No roots for given parameters."),
+    }
+
+    println!();
+    println!("***");
+    println!();
 }
 
 fn main() {
     println!("Quadratic equation solver");
-    println!("");
+    println!();
     print_solver(4.0, 3.0, 2.0);
     print_solver(1.0, -8.0, 5.0);
     print_solver(-4.0, 12.0, -9.0);
