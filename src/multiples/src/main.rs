c2fb647e43e488dd44594659bc61098fe61c6512
Adam Burucs <adam.burucs@gmail.com>
2023-12-23 14:40:35 +0100

    refactor multiples to use result

--- src/multiples/src/main.rs
+++ src/multiples/src/main.rs
@@ -4,9 +4,9 @@
 // The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-fn multiples(n: u32) -> u32 {
-    if n < 1 {
-        return 0;
+fn multiples(n: u32) -> Result<u32, String> {
+    if n < 3 {
+        return Err(String::from("Error: parameter should be at least 3."));
     }
     let mut sum = 0;
     for i in 1..n {
@@ -14,11 +14,14 @@ fn multiples(n: u32) -> u32 {
             sum += i;
         }
     }
-    sum
+    Ok(sum)
 }
 
 fn main() {
     println!("Multiples of 3 and 5");
     let sum_multiples = multiples(1000);
-    println!("The sum is {}", sum_multiples);
+    match sum_multiples {
+        Ok(val) => println!("The sum is {val}"),
+        Err(e) => println!("{e}"),
+    }
 }
