38635d8d63615a7a8177992bf3d0132b9742cc5a
Adam Burucs <adam.burucs@gmail.com>
2023-12-23 13:53:38 +0100

    refactor calc pi to use result

--- src/calculate_pi/src/main.rs
+++ src/calculate_pi/src/main.rs
@@ -7,15 +7,29 @@
 //
 // Ported to Rust by Adam Burucs in 2023.
 //
-fn calculate_pi(mut pi: f32, mut n: f32, mut sign: f32) -> f32 {
+fn calculate_pi(mut pi: f32, mut n: f32, mut sign: f32) -> Result<f32, String> {
+    if pi < 3.0 || n < 2.0 || sign < 1.0 {
+        return Err(String::from("Error: wrong init values. Inputs should be at least these or greater (3.0, 2.0, 1.0 respectively)."));
+    }
     for _i in 0..1000000 {
         pi += sign * (4.0 / ((n) * (n + 1.0) * (n + 2.0)));
         sign *= -1.0;
         n += 2.0;
     }
-    pi
+    Ok(pi)
 }
 
 fn main() {
-    println!("{}", calculate_pi(3.0, 2.0, 1.0));
+    println!("Calculate Pi using Nilkantha’s series");
+    println!();
+    println!("calculate_pi(3.0, 2.0, 1.0)");
+    let pi_calc = calculate_pi(3.0, 2.0, 1.0);
+    match pi_calc {
+        Ok(val) => {
+            println!("{val}");
+        }
+        Err(e) => {
+            println!("{e}");
+        }
+    }
 }
