dc7beb30b81c7071ae614a1c37b0c9a721e12277
Adam Burucs <adam.burucs@gmail.com>
2023-12-23 14:02:25 +0100

    refactor compound interest to use result

--- src/compound_interest/src/main.rs
+++ src/compound_interest/src/main.rs
@@ -1,30 +1,49 @@
 #[allow(dead_code)]
-fn calculate_interest(capital: u32, annual_rate: f32, years: u32) -> f32 {
+fn calculate_interest(capital: u32, annual_rate: f32, years: u32) -> Result<f32, String> {
+    if capital < 1 || annual_rate < 1.0 || years < 1 {
+        return Err(String::from(
+            "Error: each parameters should be at least 1 or greater.",
+        ));
+    }
     let mut interest = capital as f32;
     for _y in 0..years {
         interest *= 1.0 + (annual_rate / 100.0);
     }
-    interest
+    Ok(interest)
 }
 
 #[allow(dead_code)]
-fn calculate_yearly_interest(capital: u32, annual_rate: f32, years: u32) -> Vec<f32> {
+fn calculate_yearly_interest(
+    capital: u32,
+    annual_rate: f32,
+    years: u32,
+) -> Result<Vec<f32>, String> {
+    if capital < 1 || annual_rate < 1.0 || years < 1 {
+        return Err(String::from(
+            "Error: each parameters should be at least 1 or greater.",
+        ));
+    }
     let mut yearly_interests = Vec::new();
     let mut interest = capital as f32;
     for _y in 0..years {
         interest *= 1.0 + (annual_rate / 100.0);
         yearly_interests.push(interest);
     }
-    yearly_interests
+    Ok(yearly_interests)
 }
 
 fn print_interests(capital: u32, annual_rate: f32, years: u32) {
     const CURRENCY: &str = "$";
     let yearly_interests = calculate_yearly_interest(capital, annual_rate, years);
-    println!("Let's calculate the following");
-    println!("{CURRENCY}{capital} capital with {annual_rate}% over {years} years");
-    for yearly_interest in &yearly_interests {
-        println!("{CURRENCY}{yearly_interest:.2}");
+    match yearly_interests {
+        Ok(val) => {
+            println!("Let's calculate the following");
+            println!("{CURRENCY}{capital} capital with {annual_rate}% over {years} years");
+            for yearly_interest in val {
+                println!("{CURRENCY}{yearly_interest:.2}");
+            }
+        }
+        Err(e) => println!("{e}"),
     }
 }
 
