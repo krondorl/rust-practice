c41c9e4b8b1f93e86cb96e681e4ca5a97a4be45c
Adam Burucs <adam.burucs@gmail.com>
2023-12-23 13:36:50 +0100

    refactor fibonacci to use result

--- src/fibonacci/src/main.rs
+++ src/fibonacci/src/main.rs
@@ -1,19 +1,30 @@
-fn fibonacci(n: u32) -> Vec<u32> {
+fn fibonacci(n: u32) -> Result<Vec<u32>, String> {
     let mut fib_numbers = vec![1, 1];
-    let mut prev_prev_index = 0;
-    if n < 3 {
-        return fib_numbers;
+    let _prev_prev_index = 0;
+    match n {
+        0 => {
+            return Err(String::from(
+                "Error: Wrong input number, it should be greater or equal than 1.",
+            ))
+        }
+        1 | 2 => return Ok(fib_numbers),
+        3_u32..=u32::MAX => {
+            for (prev_prev_index, _i) in (3..n).enumerate() {
+                fib_numbers.push(fib_numbers[prev_prev_index] + fib_numbers[prev_prev_index + 1]);
+            }
+        }
     }
-    for _i in 3..n {
-        fib_numbers.push(fib_numbers[prev_prev_index] + fib_numbers[prev_prev_index + 1]);
-        prev_prev_index += 1;
-    }
-    fib_numbers
+    Ok(fib_numbers)
 }
 
 fn main() {
     println!("Fibonacci sequence in Rust");
     let fib_numbers = fibonacci(10);
-    let s = format!("{:?}", &fib_numbers);
-    println!("{}", s);
+    match fib_numbers {
+        Ok(val) => {
+            let s = format!("{val:?}");
+            println!("{s}");
+        }
+        Err(e) => println!("{e}"),
+    }
 }
